c37df7426d8e24b8b7f71cdbb955261c1e4d087d
Lucas Ribeiro <lucas2001.789@gmail.com>
2021-11-07 19:20:54 -0300

    finalizando landing page]

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,8 +1,14 @@
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { HomeComponent } from './components/home-component/home-component.component';
+import { CommonModule } from '@angular/common';
+import { HttpClientModule } from '@angular/common/http';
 import { NgModule } from '@angular/core';
+import { ReactiveFormsModule } from '@angular/forms';
 import { BrowserModule } from '@angular/platform-browser';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { ToastrModule } from 'ngx-toastr';
+
 
 
 @NgModule({
@@ -12,6 +18,11 @@ import { BrowserModule } from '@angular/platform-browser';
   ],
   imports: [
     BrowserModule,
+    HttpClientModule,
+    CommonModule,
+    BrowserAnimationsModule,
+    ToastrModule.forRoot(),
+    ReactiveFormsModule,
     AppRoutingModule
   ],
   providers: [],
--- src/app/components/home-component/home-component.component.ts
+++ src/app/components/home-component/home-component.component.ts
@@ -1,11 +1,29 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { FormBuilder, FormGroup, Validators } from '@angular/forms';
+import { ToastrService } from 'ngx-toastr';
 
+const EMAIL_REGEX = new RegExp(/^(([^<>()\[\]\\.,;:\s@"]+(\.[^<>()\[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.\d{1,3}\.\d{1,3}\.\d{1,3}])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/);
 @Component({
-  selector: 'ark-home-component',
+  selector: 'ark-home',
   templateUrl: './home-component.component.html',
   styleUrls: [ './home-component.component.scss' ]
 })
-export class HomeComponent {
+export class HomeComponent implements OnInit {
 
+  public form: FormGroup | any;
 
+  constructor(private readonly fb: FormBuilder, private readonly toastr: ToastrService) {}
+
+  public ngOnInit(): void {
+    this.form = this.fb.group({
+      email: [ null, Validators.pattern(EMAIL_REGEX) ]
+    })
+  }
+
+  public send(): void {
+    if (this.form.valid && this.form.get('email').value) {
+      this.toastr.success('Contato cadastrado com sucesso!');
+      this.form.reset();
+    }
+  }
 }
